8c47e9e96505b9feb38fcf08c6e15c66a5ef2bfa
Anthony K Gross <anthony.k.gross@pepperstone.com>
2017-08-17 16:34:34 +1000

    Makefile instead of create-images.sh

--- create-images.sh
+++
@@ -1,4 +0,0 @@
-#/bin/sh
-
-docker build --file="docker/images/wordpress/Dockerfile" --tag="wordpress:latest" docker/images/wordpress/.
-docker build --file="docker/images/mysql/Dockerfile" --tag="mysql:latest" docker/images/mysql/.
